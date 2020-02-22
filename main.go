48aecedaaa0925b861e4e5040b8f8f36d28f1472
Michael Katica (CMG-Videa) <michael.katica@videa.tv>
2020-02-21 19:34:54 -0500

    Changed source to Anilist graphql

--- main.go
+++ main.go
@@ -1,40 +1,100 @@
 package main
 
 import (
-	"errors"
+	golist "container/list"
+	"context"
 	"fmt"
-	"log"
-	"net/http"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/machinebox/graphql"
 )
 
-func ParseUsers() error {
-	res, err := http.Get("https://myanimelist.net/users.php")
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
-		return errors.New(fmt.Sprintf("status code error: %d %s", res.StatusCode, res.Status))
-	}
+var getReviewsGQL string = `query ($page: Int = 1) {
+    Page(page: $page, perPage: 50) {
+      pageInfo {
+        total
+        currentPage
+        lastPage
+        hasNextPage
+      }
+      reviews (mediaType:ANIME) {
+        user {
+          id
+        }
+        media {
+          id
+        }
+        score
+      }
+    }
+  }`
 
-	// Load the HTML document
-	doc, err := goquery.NewDocumentFromReader(res.Body)
+type PageInfo struct {
+	Total       int  `json:"total"`
+	CurrentPage int  `json:"currentPage"`
+	Pages       int  `json:"lastPage"`
+	HasNext     bool `json:"hasNextPage"`
+}
+
+type User struct {
+	UserID int `json:"id"`
+}
+
+type Media struct {
+	MediaID int `json:"id"`
+}
+
+type Review struct {
+	Media  `json:"media"`
+	User   `json:"user"`
+	Rating int `json:"score"`
+}
+
+type Page struct {
+	PageInfo `json:"pageInfo"`
+	Reviews  []Review `json:"reviews"`
+}
+
+type AnilistReviewQueryResponse struct {
+	Page `json:"Page"`
+}
+
+func GetReviews(gql *graphql.Client) (*golist.List, error) {
+	//intial request to get pages
+	firstReq := graphql.NewRequest(getReviewsGQL)
+	firstReq.Var("page", 1)
+	listOfReviews := golist.New()
+	var respData AnilistReviewQueryResponse
+	ctx := context.Background()
+	err := gql.Run(ctx, firstReq, &respData)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	//It works. I have no idea how much of this is needed, but the output gives me user URLs and names, so I can't complain.
-	userRows := doc.Find("div,.contentWapper").Find("div,.content").Find("table").Find("td,.borderClass > div,.picSurround > a").Find("a:not(:has(*))")
-	//userprofileLinks := gList.New()
-	userRows.Each(func(i int, s *goquery.Selection) {
-		fmt.Println(s.Parent().Html())
-		fmt.Println("=================")
-	})
-	return nil
+	hasNextpage := respData.HasNext
+	for _, v := range respData.Reviews {
+		listOfReviews.PushBack(v)
+	}
+	var respDataBuf AnilistReviewQueryResponse
+	var reqBuf *graphql.Request
+	for index := respData.CurrentPage + 1; hasNextpage; index++ {
+		reqBuf = graphql.NewRequest(getReviewsGQL)
+		reqBuf.Var("page", index)
+		err = gql.Run(ctx, reqBuf, &respDataBuf)
+		if err != nil {
+			return nil, err
+		}
+		for _, v := range respDataBuf.Reviews {
+			listOfReviews.PushBack(v)
+		}
+	}
+	fmt.Printf("%+v", listOfReviews)
+	return listOfReviews, nil
 }
 
 func main() {
-	ParseUsers()
+	client := graphql.NewClient("https://graphql.anilist.co")
+	client.Log = func(s string) { fmt.Println(s) }
+	_, err := GetReviews(client)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
